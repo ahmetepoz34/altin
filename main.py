054317ed410f17f7915cc9e3b55926354c5cf068
Ahmet Epözdemir <134329809+ahmetepoz34@users.noreply.github.com>
2025-11-14 02:28:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -117,4 +117,6 @@ def bot_loop():
 
 if __name__ == "__main__":
     threading.Thread(target=bot_loop, daemon=True).start()
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
